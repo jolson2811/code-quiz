004fd440d6a201f7a075cfbd07c7becc0e91f61f
jolson2811 <jolson2811@gmail.com>
2022-03-06 16:27:24 -0700

    Add on click query, finishe looping through questions

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,46 +1,78 @@
-var user = {
-    timerCount: 120000,//2 mintues in milliseconds, total time
-    score: 0
-}
-var questions = [//Array of questions that will be answered
-    {
-        title: "Coding Quiz Challenge",
-        text: "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!",
-        buttons: ["Start Quiz"],
-        answer: 0 
-    },
-    {
-        title: "Question 1",
-        text: "What does the array prototype push do?",
-        buttons: ["adds to front of array","takes off first element of array","takes off last element of array","adds to array end"],
-        answer: 3
-    },
-    {
-        title: "Question 2",
-        text: "What does \! mean?",
-        buttons: ["tea","help","yes"],
-        answer: 2
-    },
-    {
-        title: "Question 3",
-        text: "adfasdfasdfadsf",
-        buttons: ["adsfas","asdfasd"],
-        answer: 0
-    },
-    {
-        title: "Question 4",
-        text: "afdsasdfa",
-        buttons: ["asdfasd","adsfas"],
-        answer: 1
-    },
-    {
-        title: "Question 5",
-        text: "asdfasdfa",
-        buttons: ["asdfa","asdfa"],
-        answer: 1
+$(document).ready(function () {
+    var user = {
+        timerCount: 60,
+        score: 0
     }
-];
+    var quizCurrentPosition = 0;
+    var questions = [//Array of questions that will be answered
+        {
+            title: "Coding Quiz Challenge",
+            text: "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!",
+            buttons: ['<a id="0" href="#" class="btn btn-primary">Start Quiz</a>'],
+            answer: 0
+        },
+        {
+            title: "Question 1",
+            text: "What does the array prototype push do?",
+            buttons: ['<a id="0" href="#" class="btn btn-primary mb-4">adds to front of array</a>', '<a id="1" href="#" class="btn btn-primary mb-4">takes off first element of array</a>', '<a id="2" href="#" class="btn btn-primary mb-4">takes off last element of array</a>', '<a id="3" href="#" class="btn btn-primary mb-4">adds to array end</a>'],
+            answer: 3
+        },
+        {
+            title: "Question 2",
+            text: "What does the array prototype push do?",
+            buttons: ['<a id="0" href="#" class="btn btn-primary mb-4">adds to front of array</a>', '<a id="1" href="#" class="btn btn-primary mb-4">adds to array end</a>'],
+            answer: 1
+        },
+        {
+            title: "Question 3",
+            text: "What does the array prototype push do?",
+            buttons: ['<a id="0" href="#" class="btn btn-primary mb-4">takes off first element of array</a>', '<a id="1" href="#" class="btn btn-primary mb-4">takes off last element of array</a>', '<a id="2" href="#" class="btn btn-primary mb-4">adds to array end</a>'],
+            answer: 2
+        }
+    ];
 
-$(document).ready(function(){
+    var quizTotalLength = questions.length;
+
+    setCard(quizCurrentPosition);
+
+    function setCard(position) {
+        $("#question-title").text(questions[position].title);
+        $("#question-text").text(questions[position].text);
+        var html = "";
+        for (var i = 0; i < questions[position].buttons.length; i++) {
+            html += questions[position].buttons[i];
+        }
+        $("#answer-section").html(html);
+        $("#answer-section").attr("style", "d-flex flex-column align-items-center");
+    }
+
+    $("#answer-section").on("click", ".btn", function () {
+        if(quizCurrentPosition == 0){
+            var timerInterval = setInterval(function(){
+                if(user.timerCount > 0){
+                    user.timerCount--;
+                }else{
+                    user.timerCount = 0;
+                    clearInterval(timerInterval);
+                }
+            },1000);
+        }
+        if (quizCurrentPosition > 0) {//not card one do the following
+            if (questions[quizCurrentPosition].answer == this.id) {//if the answer is the same as user click
+                user.score++;
+                alert("Correct answer");
+            } else {
+                if(user.score > 0){
+                    user.score--;
+                }
+                alert("Incorrect answer");
+            }
+        }
+        console.log("User score is", user.score);
+        quizCurrentPosition++;
+        if (quizCurrentPosition < quizTotalLength) {
+            setCard(quizCurrentPosition);
+        }
+    });
 
 });
